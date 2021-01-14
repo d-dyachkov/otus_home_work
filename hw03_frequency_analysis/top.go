e882c0b2c6b3eccdc4d219390081298ec77a0239
Denis Dyachkov <dda3004@gmail.com>
2021-01-14 20:09:45 +0700

    HW03 (#3)
    * basic frequency analyze
    
    * advanced word frequency
    
    * use raw string for pattern
    
    Co-authored-by: Denis Dyachkov <denis.dyachkov@softomate.com>

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,52 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type wordFrequency struct {
+	word      string
+	frequency int
+}
+
+var wordSplittingPattern = regexp.MustCompile(`[\s!.,?]+`)
+
+func Top10(text string) []string {
+	countionWords := analyzeWordFrequency(text)
+	sort.Slice(countionWords, func(i, j int) bool { return countionWords[i].frequency > countionWords[j].frequency })
+	return takeWords(countionWords, 10)
+}
+
+func analyzeWordFrequency(text string) []wordFrequency {
+	wordCount := countingWords(text)
+	words := make([]wordFrequency, 0, len(wordCount))
+	for word, count := range wordCount {
+		words = append(words, wordFrequency{word: word, frequency: count})
+	}
+	return words
+}
+
+func countingWords(text string) map[string]int {
+	wordCount := make(map[string]int)
+	for _, word := range wordSplittingPattern.Split(text, -1) {
+		if word == "" || word == "-" {
+			continue
+		}
+		word = strings.ToLower(word)
+		wordCount[word]++
+	}
+	return wordCount
+}
+
+func takeWords(words []wordFrequency, n int) []string {
+	if n > len(words) {
+		n = len(words)
+	}
+	result := make([]string, 0, n)
+	for _, word := range words[:n] {
+		result = append(result, word.word)
+	}
+	return result
 }
