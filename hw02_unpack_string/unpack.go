3e1bf99639fc8c37e63d51574d105945a6cf7055
Denis Dyachkov <dda3004@gmail.com>
2021-01-14 12:33:40 +0700

    HW2 (#2)
    * implementation and tests
    
    * fix comments
    
    Co-authored-by: Denis Dyachkov <denis.dyachkov@softomate.com>

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+func Unpack(packed string) (string, error) {
+	var prev rune
+	var builder strings.Builder
+	for _, sym := range packed {
+		switch {
+		case unicode.IsDigit(sym) && isLetterOrSpace(prev):
+			{
+				if val, err := strconv.Atoi(string(sym)); err == nil && val > 0 {
+					_, err = builder.WriteString(strings.Repeat(string(prev), val))
+					if err != nil {
+						return "", err
+					}
+				}
+			}
+		case unicode.IsDigit(sym) && !isLetterOrSpace(prev):
+			return "", ErrInvalidString
+		default:
+			if isLetterOrSpace(prev) {
+				if _, err := builder.WriteRune(prev); err != nil {
+					return "", err
+				}
+			}
+		}
+		prev = sym
+	}
+	if isLetterOrSpace(prev) {
+		if _, err := builder.WriteRune(prev); err != nil {
+			return "", err
+		}
+	}
+	return builder.String(), nil
+}
+
+func isLetterOrSpace(r rune) bool {
+	return unicode.IsLetter(r) || unicode.IsSpace(r)
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 type test struct {
+	message  string
 	input    string
 	expected string
 	err      error
@@ -15,40 +16,81 @@ type test struct {
 func TestUnpack(t *testing.T) {
 	for _, tst := range [...]test{
 		{
+			message:  "Basic packed string",
 			input:    "a4bc2d5e",
 			expected: "aaaabccddddde",
 		},
 		{
+			message:  "Only letters",
 			input:    "abccd",
 			expected: "abccd",
 		},
 		{
+			message:  "The length of the packed character at the beginning of the line",
 			input:    "3abc",
 			expected: "",
 			err:      ErrInvalidString,
 		},
 		{
+			message:  "Only digits",
 			input:    "45",
 			expected: "",
 			err:      ErrInvalidString,
 		},
 		{
+			message:  "Two digits in a row",
 			input:    "aaa10b",
 			expected: "",
 			err:      ErrInvalidString,
 		},
 		{
+			message:  "Empty string",
 			input:    "",
 			expected: "",
 		},
 		{
+			message:  "Packed character with length 0",
 			input:    "aaa0b",
 			expected: "aab",
 		},
+		{
+			message:  "String with packed newline character",
+			input:    "d\n5abc",
+			expected: "d\n\n\n\n\nabc",
+		},
+		{
+			message:  "Russian packed text",
+			input:    "приве0т",
+			expected: "привт",
+		},
+		{
+			message:  "The length of the packed character in the end string",
+			input:    "привет2",
+			expected: "приветт",
+		},
+		{
+			message:  "The negative length of the packed character",
+			input:    "привет-1",
+			expected: "",
+			err:      ErrInvalidString,
+		},
+		{
+			message:  "Just a space",
+			input:    " ",
+			expected: " ",
+		},
+		{
+			message:  "Different register",
+			input:    "A1a0a1",
+			expected: "Aa",
+		},
 	} {
-		result, err := Unpack(tst.input)
-		require.Equal(t, tst.err, err)
-		require.Equal(t, tst.expected, result)
+		tst := tst
+		t.Run(tst.message, func(t *testing.T) {
+			result, err := Unpack(tst.input)
+			require.Equal(t, tst.err, err)
+			require.Equal(t, tst.expected, result)
+		})
 	}
 }
 
